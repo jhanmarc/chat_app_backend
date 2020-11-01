10641a5609a73cb29f03e33f450b8e73ee1d1c7a
Jhan Marco Soto <sjhan@Jhans-MacBook-Pro.local>
2020-10-31 21:23:55 -0500

    add code correct

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -9,7 +9,6 @@ exports.login = async (req = request, res = response ) => {
         const user = await User.findOne({ email: req.body.email })
         if(!user) return res.status(400).json({ok:false, msg: 'User no found'})
         const validatePass = await User.comparePassword(user.password, req.body.password)
-        console.log(validatePass);
         if(!validatePass) return res.status(400).json({ok:false, msg: 'password no incorrect'})
         const token = await jwt.createToken(user._id);
         res.json({ok: true, token})
